2183f12b8abea4047da534b5f905f4604ed740f2
Matithyahu <matthew@metzger.cc>
2017-06-15 23:59:37 -0500

    array added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PracticalJavascript-v5
 Based on Practical Javascript by [Watch and Code](https://watchandcode.com/)
+
+Introduces "for loops"
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-for (var i = 0; i < 10; i++) {
-  console.log("Hello");
+var testArray = ["item 1", "item 2", "item 3"];
+
+for (var i = 0; i < testArray.length; i++) {
+  console.log(testArray[i]);
 }
